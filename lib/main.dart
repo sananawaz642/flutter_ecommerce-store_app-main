b5fc52845aaa8c2e7c2fcc6d383d9a894c6426f3
Chandio763 <kashikha763@gmail.com>
2024-05-01 21:36:01 +0500

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:e_commerce_flutter/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui' show PointerDeviceKind;
 import 'package:e_commerce_flutter/core/app_theme.dart';
 import 'package:e_commerce_flutter/src/view/screen/home_screen.dart';
 
-void main() => runApp(const MyApp());
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
